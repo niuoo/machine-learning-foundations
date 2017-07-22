7e6a022e677565986e047958e9e51a04b5ba2d9c
yuanyuan.niu <yuanyuan.niu@shuyun.com>
2017-07-22 15:57:07 +0800

    pocket算法

--- foundations/pocket_algorithm.py
+++ foundations/pocket_algorithm.py
@@ -8,14 +8,14 @@ train_data = pd.read_csv('https://www.csie.ntu.edu.tw/~htlin/mooc/datasets/mlfou
 test_data = pd.read_csv('https://www.csie.ntu.edu.tw/~htlin/mooc/datasets/mlfound_math/hw1_18_test.dat', header=None,
                         sep="\s|\t", engine='python')
 
-YTRAIN = train_data[4]
-LEN = len(YTRAIN)
-MXTRAIN = np.mat(train_data[[0, 1, 2, 3]])  # 将前4列数据矩阵化
-MXTRAIN = np.c_[np.ones(LEN), MXTRAIN]  # 增加第一列全部为1
+Y = train_data[4]
+N = len(Y)
+X = np.mat(train_data[[0, 1, 2, 3]])  # 将前4列数据矩阵化
+X = np.c_[np.ones(N), X]  # 增加第一列全部为1
 
 YTEST = test_data[4]
 MXTEST = np.mat(test_data[[0, 1, 2, 3]])  # 将前4列数据矩阵化
-MXTEST = np.c_[np.ones(LEN), MXTEST]  # 增加第一列全部为1
+MXTEST = np.c_[np.ones(N), MXTEST]  # 增加第一列全部为1
 
 
 def error_sum(w, X, Y):
@@ -29,45 +29,42 @@ def error_sum(w, X, Y):
 
 def train(pocket=True, update=50):
     w = np.zeros(5)
-    error = error_sum(w, MXTRAIN, YTRAIN)
+    error = error_sum(w, X, Y)
     w_pocket = w
     cnt = 0
     while cnt < update:
-        idx = random.sample(range(LEN), LEN)  # 打乱循环的次序
+        idx = random.sample(range(N), N)  # 打乱循环的次序
         for i in idx:
-            if np.sign(w * MXTRAIN[i].T) != YTRAIN[i]:
-                w = w + YTRAIN[i] * MXTRAIN[i]
-                if not pocket:
-                    cnt += 1
-                else:
-                    e = error_sum(w, MXTRAIN, YTRAIN)
+            if np.sign(w * X[i].T) != Y[i]:
+                w = w + Y[i] * X[i]
+                if pocket:
+                    e = error_sum(w, X, Y)
                     if e < error:
                         error = e
                         w_pocket = w
                     cnt += 1
                     break
+                else:
+                    cnt += 1
+
     if pocket:
-        return error_sum(w_pocket, MXTEST, YTEST)/float(len(YTEST))
+        return error_sum(w_pocket, MXTEST, YTEST) / float(len(YTEST))
     else:
-        return error_sum(w, MXTEST, YTEST)/float(len(YTEST))
+        return error_sum(w, MXTEST, YTEST) / float(len(YTEST))
 
 
 def pre_random(n, pocket=True, update=50):
     error_rate = 0
     for i in xrange(n):
-        e_rate = train(pocket, update);
-        error_rate += e_rate
-        # print e
-
+        error_rate += train(pocket, update)
     return error_rate / n
 
 
 def main():
-
     # The process will last for  several minutes
-    print pre_random(200, True, 50)  # question 18 , output: 0.1352
-    print pre_random(200, False, 50)  # question 19 , output: 0.2518
-    print pre_random(200, True, 100)  # question 20 , output: 0.1212
+    print pre_random(20, True, 50)  # question 18 , output: 0.13508
+    print pre_random(20, False, 50)  # question 19 , output: 0.23189
+    print pre_random(20, True, 100)  # question 20 , output: 0.1181
 
 
 if __name__ == '__main__':
